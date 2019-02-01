a4506c4768f1dfff52e6b79c785a0fa0d8647282
tlinkner <todd@toddlinkner.com>
2019-02-01 15:03:40 -0500

    in-class simple examples

---
+++ webpack-in-class/src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+import {testFunction, var1 as var2} from "./testModule";
+import * as d3 from 'd3';
+
+const var1 = 10;
+
+// console.log("Hi from index.js");
+
+console.log(testFunction(4));
+console.log(var1);
+console.log(var2);
+console.log(d3);
